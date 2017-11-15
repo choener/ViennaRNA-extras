da024ef53b3d3370683913ae476e4250e8e79e0d
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2017-11-15 22:02:52 +0100

    added high-level RNAeval wrapper

---
+++ BioInf/ViennaRNA/RNAeval.hs
@@ -0,0 +1,20 @@
+
+module BioInf.ViennaRNA.RNAeval where
+
+import Biobase.Types.Energy (DG(..))
+import Biobase.Types.Sequence (RNAseq(..))
+import Biobase.Types.Structure (RNAss(..))
+import BioInf.ViennaRNA.Bindings (eosTemp)
+
+import BioInf.ViennaRNA.Internal
+
+
+
+-- | High-level wrapper for energy-of-struct calculations similar to the
+-- command line @RNAeval@.
+
+rnaeval ∷ Double → RNAseq → RNAss → DG
+rnaeval t (RNAseq s1) (RNAss s2)
+  = unsafePerformIO . withMutex
+  $ DG <$> eosTemp t s1 s2
+
