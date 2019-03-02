9ce7331f28564ba8e20a714b58e844384e88f736
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-03-02 23:41:59 +0100

    move to envhs

--- default.nix
+++ default.nix
@@ -1,2 +1,23 @@
-with (import <nixpkgs> {});
-hsDevFunctions ./.
+{ mkDerivation, array, attoparsec, base, BiobaseTypes, BiobaseXNA
+, bytestring, data-default-class, deepseq, lens, parallel
+, QuickCheck, stdenv, streaming, streaming-bytestring, strict
+, strict-base-types, tasty, tasty-quickcheck, tasty-th, vector
+, ViennaRNA-bindings
+}:
+mkDerivation {
+  pname = "ViennaRNA-extras";
+  version = "0.0.0.1";
+  src = ./.;
+  libraryHaskellDepends = [
+    array attoparsec base BiobaseTypes BiobaseXNA bytestring
+    data-default-class deepseq lens QuickCheck streaming
+    streaming-bytestring strict strict-base-types ViennaRNA-bindings
+  ];
+  testHaskellDepends = [
+    attoparsec base BiobaseTypes bytestring data-default-class lens
+    parallel QuickCheck tasty tasty-quickcheck tasty-th vector
+  ];
+  homepage = "https://github.com/choener/ViennaRNA-extras";
+  description = "ViennaRNA v2 extensions";
+  license = stdenv.lib.licenses.bsd3;
+}
